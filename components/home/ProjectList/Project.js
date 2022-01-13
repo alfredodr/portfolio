93c832ebfd9e20f30f070fcafa550f2fa9be8b81
alfredodr <aldodigitalmax@gmail.com>
2022-01-12 23:08:02 -0500

    Fix project image to fill container. Fix project cards styles.

--- components/home/ProjectList/Project.js
+++ components/home/ProjectList/Project.js
@@ -5,18 +5,41 @@ import { BiLinkExternal } from "react-icons/bi";
 
 const Project = ({ data }) => {
   const { name, id, img, } = data;
+
+  let i=0;
+  const details=id=>{ 
+    for(i; i<=id; i++){
+      console.log(i);
+      return `styles.detail${i}`;
+    }
+  }  
+
+
+
   return (
     <>
-      <div className={id === 1 ? styles.project1 : styles.project2} >
+      <div className={
+          id === 1 ? styles.project1 : 
+          id === 2 ? styles.project2 : 
+          id === 3 ? styles.project3 : null
+      }>
         <Image
-          className={id === 1 ? styles.image1 : styles.image1} src={img} placeholder="blur" blurDataURL={"/blank.jpg"}
+          className={
+            id === 1 ? styles.image1 : 
+            id === 2 ? styles.image2 : 
+            id === 3 ? styles.image3 : null
+          } 
+          src={img} placeholder="blur" blurDataURL={"/blank.jpg"}
           alt="Find an Agent Project for Client"
           width="500px"
           height="400px" />
         <a
           target="blank"
           href={id === 1 ? "https://www.univistainsurance.com/location/univista-insurance-hialeah-fl-33010-2/" : ""}
-          className={id === 1 ? styles.detail1 : styles.detail2}>
+          className={
+            id === 1 ? styles.detail1 : 
+            id === 2 ? styles.detail2 : 
+            id === 3 ? styles.detail3 : null}>
           Details
           <BiLinkExternal className={styles.external_link} size="1.5rem" />
         </a>
@@ -27,3 +50,5 @@ const Project = ({ data }) => {
 };
 
 export default Project;
+
+//className={id === 1 ? styles.detail1 : styles.detail2}>
